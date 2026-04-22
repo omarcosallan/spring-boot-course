834cab8e91c70880391975dec8e89e7d0f8cc99f
Marcos <marcosallanm1to@gmail.com>
2026-04-22 16:14:43 -0300

    sbfs-20-hands-on-tight-loose

---
+++ src/main/java/com/loose/coupling/LooseCouplingExample.java
@@ -0,0 +1,19 @@
+package com.loose.coupling;
+
+import java.sql.SQLOutput;
+
+public class LooseCouplingExample {
+    public static void main(String[] args) {
+        UserDataProvider databaseProvider = new UserDatabaseProvider();
+        UserManager userManagerWithDB = new UserManager(databaseProvider);
+        System.out.println(userManagerWithDB.getUserInfo());
+
+        UserDataProvider webServiceProvider = new WebServiceDataProvider();
+        UserManager userManagerWithWS = new UserManager(webServiceProvider);
+        System.out.println(userManagerWithWS.getUserInfo());
+
+        UserDataProvider newDatabaseProvider = new UserDatabaseProvider();
+        UserManager userManagerWithNewDB = new UserManager(newDatabaseProvider);
+        System.out.println(userManagerWithNewDB.getUserInfo());
+    }
+}
