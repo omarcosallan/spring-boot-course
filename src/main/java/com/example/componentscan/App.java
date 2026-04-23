dfb1bc1b45c13d9645015fd12092211485e247be
Marcos <marcosallanm1to@gmail.com>
2026-04-23 20:04:20 -0300

    sbfs-38-value-annotation

--- src/main/java/com/example/componentscan/App.java
+++ src/main/java/com/example/componentscan/App.java
@@ -1,4 +1,4 @@
-package com.example.componentScan;
+package com.example.componentscan;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
