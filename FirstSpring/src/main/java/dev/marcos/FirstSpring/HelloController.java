4bf682bf9984ccc60f4de04711a251d20bf2f81f
Marcos <marcosallanm1to@gmail.com>
2026-04-26 15:45:01 -0300

    sbfs-60.1-json-response

--- FirstSpring/src/main/java/dev/marcos/FirstSpring/HelloController.java
+++ FirstSpring/src/main/java/dev/marcos/FirstSpring/HelloController.java
@@ -1,13 +1,20 @@
 package dev.marcos.FirstSpring;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello World!";
+    public HelloResponse hello() {
+        return new HelloResponse("Hello World!");
+    }
+
+    @PostMapping("/hello")
+    public HelloResponse helloPost(@RequestBody String name) {
+        return new HelloResponse("Hello " + name + "!");
     }
 }
