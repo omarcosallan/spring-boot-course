b0705bebd93d64310913aa9e93c74e14a293ced7
Marcos <marcosallanm1to@gmail.com>
2026-04-27 18:20:55 -0300

    sbfs-62-setting-up-project

---
+++ sb-ecom/src/main/java/com/ecommerce/project/SbEcomApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SbEcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbEcomApplication.class, args);
+	}
+}
