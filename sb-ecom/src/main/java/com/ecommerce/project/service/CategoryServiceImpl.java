84f7cbf3a83de33f98c011401227c84d08b1ddad
Marcos <marcosallanm1to@gmail.com>
2026-05-03 11:40:31 -0300

    sbfs-103-validation-no-categories

--- sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -19,7 +19,11 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     public List<Category> getAllCategories() {
-        return categoryRepository.findAll();
+        List<Category> categories = categoryRepository.findAll();
+        if (categories.isEmpty()) {
+            throw new APIException("No category created till now");
+        }
+        return categories;
     }
 
     @Override
