6833dbfaad14ef0ae4bd84ac9e4d7ae7205a6a38
Marcos <marcosallanm1to@gmail.com>
2026-05-09 13:06:50 -0300

    sbfs-143-saving-products-category

--- sb-ecom/src/main/java/com/ecommerce/project/controller/ProductController.java
+++ sb-ecom/src/main/java/com/ecommerce/project/controller/ProductController.java
@@ -1,9 +1,24 @@
 package com.ecommerce.project.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ecommerce.project.model.Product;
+import com.ecommerce.project.payload.ProductDTO;
+import com.ecommerce.project.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping("/admin/categories/{categoryId}/product")
+    public ResponseEntity<ProductDTO> addProduct(@RequestBody Product product,
+                                                      @PathVariable Long categoryId) {
+        ProductDTO productDTO = productService.addProduct(categoryId, product);
+        return new ResponseEntity<>(productDTO, HttpStatus.CREATED);
+    }
 }
