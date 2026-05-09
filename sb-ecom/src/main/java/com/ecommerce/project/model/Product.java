986da7123af7b13ee533b5464f3d8dd6003de5df
Marcos <marcosallanm1to@gmail.com>
2026-05-09 12:04:17 -0300

    sbfs-141-setting-product-module

--- sb-ecom/src/main/java/com/ecommerce/project/model/Product.java
+++ sb-ecom/src/main/java/com/ecommerce/project/model/Product.java
@@ -16,7 +16,7 @@ public class Product {
     private Long productId;
     private String productName;
     private String description;
-    private Long quantity;
+    private Integer quantity;
     private double price;
     private double specialPrice;
 
