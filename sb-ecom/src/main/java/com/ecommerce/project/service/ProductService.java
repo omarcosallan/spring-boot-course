e55b163ff5729f5494c66d21788c1fb3def21bdb
Marcos <marcosallanm1to@gmail.com>
2026-05-11 07:00:53 -0300

    sbfs-153-challenge-pagination-sorting

--- sb-ecom/src/main/java/com/ecommerce/project/service/ProductService.java
+++ sb-ecom/src/main/java/com/ecommerce/project/service/ProductService.java
@@ -8,9 +8,9 @@ import java.io.IOException;
 
 public interface ProductService {
     ProductDTO addProduct(Long categoryId, ProductDTO productDTO);
-    ProductResponse getAllProducts();
-    ProductResponse searchByCategory(Long categoryId);
-    ProductResponse searchByKeyword(String keyword);
+    ProductResponse getAllProducts(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
+    ProductResponse searchByCategory(Long categoryId, Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
+    ProductResponse searchByKeyword(String keyword, Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
     ProductDTO updateProduct(Long productId, ProductDTO productDTO);
     ProductDTO deleteProduct(Long productId);
     ProductDTO updateProductImage(Long productId, MultipartFile image) throws IOException;
